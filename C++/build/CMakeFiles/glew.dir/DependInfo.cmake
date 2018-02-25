56edca69b52bc5bd7e856288bf6230d543e26bd6
dcleres <david.cleres@epfl.ch>
2018-02-25 10:58:28 +0100

    C++ folder problems solved (??)

---
+++ C++/build/CMakeFiles/glew.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/edoardoremelli/Desktop/libigl/external/nanogui/ext/glew/src/glew.c" "/Users/edoardoremelli/Desktop/libigl/example-p/libigl-example-project/build/CMakeFiles/glew.dir/Users/edoardoremelli/Desktop/libigl/external/nanogui/ext/glew/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_BUILD"
+  "GLEW_NO_GLU"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/edoardoremelli/Desktop/libigl/shared/cmake/../../external/nanogui/ext/glew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
