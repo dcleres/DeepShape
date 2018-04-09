e6bbcfcb687b599e6e60c830a8c16e7598f96de5
dcleres <david.cleres@epfl.ch>
2018-04-09 10:43:22 +0200

    Cleaned Repo

--- voxels/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/davidcleres/DeepShape/voxels
-# Build directory: /Users/davidcleres/DeepShape/voxels/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("bin")
-subdirs("test")
--- voxels/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/davidcleres/DeepShape/voxels/test
-# Build directory: /Users/davidcleres/DeepShape/voxels/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(collisionChecker "/collisionChecker")
