047119ad54e5707e2fe584cc1415edae3dea7b8e
dcleres <david.cleres@epfl.ch>
2018-06-01 00:47:49 +0200

    batch sizes

---
+++ DeepLearningPolycubing/Activation-ReLu/Python Files - 4 Filters/Conv3D - Polycubes-Grid_size32-CE-Sigmoid-Adamax-BS25.py
@@ -0,0 +1,188 @@
+import matplotlib
+matplotlib.use("nbagg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+from utility import *
+from models import *
+
+import math
+
+import torch
+import torch.nn as nn
+import torch.utils as utils
+import torch.nn.init as init
+import torch.utils.data as data
+import torchvision.utils as v_utils
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from torch import Tensor
+
+import torch.optim as optim
+import torch.nn.functional as F
+
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+onCluster = True
+
+if onCluster:
+    polycube_path = "/home/cleres/anaconda3/DeepShape/Polycubing-Automated/Generated-Cars/"
+    polycube_files = [f for f in listdir(polycube_path) if isfile(join(polycube_path, f))]
+    
+    voxelized_mesh_path = "/home/cleres/anaconda3/DeepShape/Polycubing-Automated/voxelizedMeshes/"
+    voxelized_mesh_files = [f for f in listdir(voxelized_mesh_path) if isfile(join(voxelized_mesh_path, f))]
+
+else:
+    polycube_path = "/Users/davidcleres/DeepShape/Polycubing-Automated/Generated-Cars/"
+    polycube_files = [f for f in listdir(polycube_path) if isfile(join(polycube_path, f))]
+    
+    voxelized_mesh_path = "/Users/davidcleres/DeepShape/Polycubing-Automated/voxelizedMeshes/"
+    voxelized_mesh_files = [f for f in listdir(voxelized_mesh_path) if isfile(join(voxelized_mesh_path, f))]
+
+voxelizedFiles = []
+polycubedFiles = []
+
+for f in voxelized_mesh_files: 
+    if f[-13:] == "voxelized.txt":
+        voxelizedFiles = np.hstack((voxelizedFiles, f))
+    
+for f in polycube_files:
+    if f[-14:] == "finalCubes.txt":
+        polycubedFiles = np.hstack((polycubedFiles, f))
+        
+#Definition of the global paramters
+grid_size=32
+batch_size=25
+
+
+# Save the tensor to a text file 
+voxelized_train_input, polycube_target=loadData(grid_size, polycube_path, voxelized_mesh_path, voxelizedFiles, polycubedFiles, loadFromScratch=True)
+
+#Create a Training and a Validation Set 
+preprocessed_input_train, preprocessed_input_validation, preprocessed_input_train_target, preprocessed_input_validation_target = preprocessing_train(voxelized_train_input, polycube_target,batch_size, False, False)
+
+preprocessed_input_train = torch.from_numpy(preprocessed_input_train)
+preprocessed_input_validation = torch.from_numpy(preprocessed_input_validation)
+preprocessed_input_train_target = torch.from_numpy(preprocessed_input_train_target)
+preprocessed_input_validation_target = torch.from_numpy(preprocessed_input_validation_target)
+
+Ntrain = len(preprocessed_input_train[:, 0,0,0,0]) 
+Nvalidation = len(preprocessed_input_validation[:,0,0,0,0])
+
+train_input = Variable(preprocessed_input_train.view(Ntrain, 1, grid_size, grid_size, grid_size).float())
+validation_input = Variable(preprocessed_input_validation.view(Nvalidation, 1, grid_size, grid_size, grid_size).float())
+
+labels_train = preprocessed_input_train_target.float()
+labels_validation = preprocessed_input_validation_target.float()
+
+print('train', train_input.shape)
+print('validation', validation_input.shape)
+print('train_target', labels_train.shape)
+print('validation_target', labels_validation.shape)
+
+#Init the neural network 
+# Train network 
+#criterion = nn.BCELoss()
+criterion = nn.CrossEntropyLoss()
+#criterion = nn.PoissonNLLLoss()
+#criterion = nn.BCEWithLogitsLoss()
+#criterion = nn.SmoothL1Loss() #interesting ... but does not converge
+#criterion = nn.MSELoss() #0.83 but unstable
+
+if isinstance(criterion, nn.CrossEntropyLoss):
+    train_target = Variable(preprocessed_input_train_target).long()  # keep long tensors
+    validation_target = Variable(preprocessed_input_validation_target, volatile=True).long() # convert to float
+    Noutputs = 2
+    
+elif isinstance(criterion, nn.NLLLoss):
+    train_target = Variable(preprocessed_input_train_target)  # keep long tensors
+    validation_target = Variable(preprocessed_input_validation_target, volatile=True) # convert to float
+    Noutputs = 2
+    
+else:
+    train_target = Variable(preprocessed_input_train_target.float()) # convert to float
+    validation_target = Variable(preprocessed_input_validation_target.float(), volatile=True ) # convert to float
+    Noutputs = 1
+    
+Nbatches = int(math.ceil(Ntrain/batch_size)) #batch_size is defined above
+Nepochs = 500
+Nrep = 1
+        
+#model = conv3DNet(grid_size, Noutputs, batch_size)
+#model = conv3DNet(grid_size, Noutputs, batch_size)
+#model = conv3DNet(grid_size, Noutputs, batch_size)
+#model = conv3DNet(grid_size, Noutputs, batch_size)
+model = UnetGenerator_3d(in_dim=1, out_dim=Noutputs, num_filter=4)
+
+#optimizer = optim.SGD(model.parameters(), lr=1e-2, momentum=0.90)
+#optimizer = optim.Adam(model.parameters())
+#optimizer = optim.Adagrad(model.parameters())
+optimizer = optim.Adamax(model.parameters())
+#optimizer = optim.ASGD(model.parameters())
+#optimizer = optim.RMSprop(model.parameters())
+#optimizer = optim.Rprop(model.parameters())
+ 
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10, verbose=True) #Reduces the learning rate if it did not decreased by more than 10^-4 in 10 steps
+
+train_errors = torch.Tensor(Nepochs).zero_()
+validation_errors = torch.Tensor(Nepochs).zero_()
+
+ep_loss = torch.Tensor(Nepochs).zero_()
+
+for i_ep in range(Nepochs):
+    for b_start in range(0, Ntrain, batch_size):
+        bsize_eff = batch_size - max(0, b_start+batch_size-Ntrain)  # boundary case
+        model.train()
+        model.zero_grad()
+        output = model(train_input.narrow(0, b_start, bsize_eff))
+        if isinstance(criterion, nn.CrossEntropyLoss) or isinstance(criterion, nn.NLLLoss):
+            batch_loss = criterion(output, train_target.narrow(0, b_start, bsize_eff))
+        else:
+            #if delta model is chosen
+            #batch_loss = criterion(output.view(bsize_eff*Noutputs), train_target.narrow(0, b_start, bsize_eff))
+            batch_loss = criterion(output.view(bsize_eff,grid_size,grid_size,grid_size), train_target.narrow(0, b_start, bsize_eff))
+        ep_loss[i_ep] += batch_loss.data[0]
+        batch_loss.backward()
+        optimizer.step()
+
+    scheduler.step(ep_loss[i_ep])
+
+    nb_train_errs = compute_nb_errors(model, train_input, train_target, batch_size, criterion)
+    nb_validation_errs = compute_nb_errors(model, validation_input, validation_target, batch_size, criterion)
+
+    Ntrain_nb = Ntrain*grid_size**3
+    Nvalidation_nb = Nvalidation*grid_size**3
+    print("Epoch Number : ", i_ep)
+    print("\t Training accuracy: ", (100*(Ntrain_nb-nb_train_errs)/Ntrain_nb))
+    print("\t Validation accuracy ",(100*(Nvalidation_nb-nb_validation_errs)/Nvalidation_nb))
+
+    print("\t Epoch Loss ", ep_loss[i_ep])
+
+    train_errors[i_ep] = nb_train_errs
+    validation_errors[i_ep] = nb_validation_errs
+    
+training_accuracy = np.array(100*(Ntrain_nb-train_errors)/Ntrain_nb)
+validation_accurcy = np.array(100*(Nvalidation_nb-validation_errors)/Nvalidation_nb)
+np.save('training_accuracy_Adamax', training_accuracy)
+np.save('validation_accuracy_Adamax', validation_accurcy)
+
+plt.plot(training_accuracy)
+plt.plot(validation_accurcy)
+plt.savefig('Conv3D - Polycubes-Grid_size32-CE-Sigmoid-Adamax.png')
+plt.show()
+
+test_visualisation = output[14,1,:,:,:].round()
+
+voxels = np.array(test_visualisation.data)
+
+# and plot everything
+fig = plt.figure(figsize=(10,10))
+ax = fig.gca(projection='3d')
+ax.voxels(voxels)
+fig.savefig('VoxelizedFinal.png')
+fig.show()
--- DeepLearningPolycubing/models.py
+++ DeepLearningPolycubing/models.py
@@ -12,89 +12,32 @@ from torch import Tensor
 import torch.optim as optim
 import torch.nn.functional as F
 
-# DC U-Net
-class conv3DNet_1(nn.Module):
-    def __init__(self, gridSize, output_units, batch_size):
-        """Initializes neural network with 3 convolutional layers and 1 fully-connected layer.
+#find the coordinates of the center and the deltas 
+class conv2DNet_1(nn.Module):
+    def __init__(self, output_units):
+        super(conv2DNet_1, self).__init__()
         
-        Args:
-            - Nchannels (int): number of EEG channels
-            - Nsamples (int): number of time points in each EEG signal
-            - output_units (int): number of output units, e.g. 1 for training with loss torch.nn.BCELoss or 2 with 
-            loss torch.nn.CrossEntropyLoss            
-            """
-        super(conv3DNet, self).__init__()
+        #4*1*54 for 125 HZ 
+        #4*1*42 for 100 Hz signal 
+        self.fc_inputs = 16*20*20*20
         
-        Noutputs = 1
-        self.fc_linear = 1020 * gridSize**3
-        self.batch_size = batch_size
-        self.output_units=output_units
-        
-        '''# Layer 1
-        l1_channels = 16
-        self.conv1 = nn.Conv3d(1, l1_channels, kernel_size=3, padding = 1)
-        self.batchnorm1 = nn.BatchNorm3d(l1_channels, False) # final size bsize x 1 x l1_channels x Nsamples
-        self.pooling1 = nn.MaxPool3d(kernel_size=2, padding=1)'''
-        
-        # Layer 2
-        l2_channels = 32
-        self.conv2 = nn.Conv3d(1, l2_channels, kernel_size=3, padding = 1, stride = 1)
-        self.batchnorm2 = nn.BatchNorm3d(l2_channels, False) # final size bsize x 1 x l1_channels x Nsamples
-        self.pooling2 = nn.MaxPool3d(kernel_size=2)
-        
-        '''# Layer 3
-        l3_channels = 64
-        self.conv3 = nn.Conv3d(l2_channels, l3_channels, kernel_size=3, padding = 1, stride = 1)
-        self.batchnorm3 = nn.BatchNorm3d(l3_channels, False) # final size bsize x 1 x l1_channels x Nsamples
-        self.pooling3 = nn.MaxPool3d(kernel_size=2)
-        
-        self.up1 = nn.ConvTranspose3d(l3_channels, l2_channels, kernel_size=2, stride=2)'''
-        self.up2 = nn.ConvTranspose3d(l2_channels, self.output_units,kernel_size=2, stride=2)
-    
-    def forward(self, x):
-        """Applies forward pass consisting of 3 convolutional layers followed by a fully-connected linear layer.
-        
-        Args:
-            - x (torch.autograd.Variable): the input batch. It has dimension batch_size x Nchannel x Nsamples x 1,
-            where Nchannel is the number of EEG channels and Nsamples the number of time points.
-        
-        Returns:
-            - (torch.autograd.Variable) of size either batch_size x output_units   
-        
-        """ 
-        print(x.shape)
+        self.conv = torch.nn.Sequential()
+        self.conv.add_module("conv_1", torch.nn.Conv3d(1, 8, kernel_size=5, dilation=2))
+        self.conv.add_module("BN_1", torch.nn.BatchNorm3d(8, False))
+        self.conv.add_module("conv_2", torch.nn.Conv3d(8, 16, kernel_size=5))
+        self.conv.add_module("BN_2", torch.nn.BatchNorm3d(16, False))
 
-        '''# Layer 1
-        x = F.relu(self.conv1(x))              # bsize x l1_channels x 1 x Nsamples
-        x = self.batchnorm1(x)
-        x = F.dropout(x, 0.3)
-        #x = self.pooling1(x)
-        print(x.shape)'''
-
-        # Layer 2
-        x = F.relu(self.conv2(x))              # bsize x l2_channels x l1_channels x Nsamples
-        x = self.batchnorm2(x)       
-        x = F.dropout(x, 0.3)
-        x = self.pooling2(x)                  # bsize x l2_channels x floor(l1_channels/2) x floor(Nsamples/4)
-        print(x.shape)
+        self.fc = torch.nn.Sequential()
+        self.fc.add_module("fc1", torch.nn.Linear(self.fc_inputs, output_units))
+        #self.fc.add_module("relu_3", torch.nn.ReLU())
+        #self.fc.add_module("dropout_3", torch.nn.Dropout3d(0.2))
+        #self.fc.add_module("fc2", torch.nn.Linear(16, output_units)) #add batch_norms
+        #self.fc.add_module("sig_1", torch.nn.Sigmoid()) 
         
-        '''# Layer 3
-        #x = self.padding1(x)
-        x = F.relu(self.conv3(x))              # bsize x l2_channels x l1_channels x Nsamples
-        x = self.batchnorm3(x)       
-        x = F.dropout(x, 0.3)
-        x = self.pooling3(x)                  # bsize x l2_channels x floor(l1_channels/2) x floor(Nsamples/4)
-        print(x.shape)
-        
-        x =  F.relu(self.up1(x))
-        print(x.shape)'''
-        x =  F.relu(self.up2(x))
-        print(x.shape)
-        x = F.softmax(x)
-        
-        x = x.view(15, self.output_units, gridSize, gridSize, gridSize)
-        
-        return x
+    def forward(self,x):     
+        x = self.conv.forward(x)
+        x = x.view(-1, self.fc_inputs)
+        return self.fc.forward(x)
 
 #find the coordinates of the center and the deltas 
 class conv2DNet_2(nn.Module):
@@ -131,6 +74,40 @@ class conv2DNet_2(nn.Module):
         #print(x.shape)
         x = x.view(-1, self.fc_inputs)
         return self.fc.forward(x)
+    
+class CNNGenerator_3d(nn.Module):
+    def __init__(self,in_dim,out_dim,num_filter):
+        super(CNNGenerator_3d,self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.num_filter = num_filter
+        act_fn = nn.LeakyReLU(0.2, inplace=True)
+
+        print("\n------Initiating CNN ------\n")
+        
+        self.fc_inputs = 16*4*4*4
+        
+        self.down_1 = conv_block_2_3d(self.in_dim,self.num_filter,act_fn)
+        self.pool_1 = maxpool_3d()
+        self.down_2 = conv_block_2_3d(self.num_filter,self.num_filter*2,act_fn)
+        self.pool_2 = maxpool_3d()
+        self.down_3 = conv_block_2_3d(self.num_filter*2,self.num_filter*4,act_fn)
+        self.pool_3 = maxpool_3d()
+        
+        self.fc = torch.nn.Sequential()
+        self.fc.add_module("fc1", torch.nn.Linear(self.fc_inputs, out_dim))        
+
+    def forward(self,x):
+        down_1 = self.down_1(x)
+        pool_1 = self.pool_1(down_1)
+        down_2 = self.down_2(pool_1)
+        pool_2 = self.pool_2(down_2)
+        down_3 = self.down_3(pool_2)
+        pool_3 = self.pool_3(down_3)
+        #print(pool_3.shape)
+        x = pool_3.view(-1, self.fc_inputs)
+        return self.fc.forward(x)
+                       
     
 #3DShapeNets from Princeton CVPR 2015
 class conv2DNet_Princeton(nn.Module):
